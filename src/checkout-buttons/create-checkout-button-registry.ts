ede831073c5109035df805ab4277d0c577b6d8f4
Irving Gomez <lordzero0000@gmail.com>
2018-09-19 11:02:39 -0500

    feat(payment): INT-838 [NGCheckout] Add Google Pay button to Cart Page

--- src/checkout-buttons/create-checkout-button-registry.ts
+++ src/checkout-buttons/create-checkout-button-registry.ts
@@ -1,14 +1,22 @@
-import { createFormPoster } from '@bigcommerce/form-poster';
+import { createFormPoster, FormPoster } from '@bigcommerce/form-poster';
 import { RequestSender } from '@bigcommerce/request-sender';
 import { getScriptLoader } from '@bigcommerce/script-loader';
 
+import { BillingAddressActionCreator, BillingAddressRequestSender } from '../billing';
 import { CheckoutActionCreator, CheckoutRequestSender, CheckoutStore } from '../checkout';
+import CheckoutValidator from '../checkout/checkout-validator';
 import { Registry } from '../common/registry';
 import { ConfigActionCreator, ConfigRequestSender } from '../config';
+import { OrderActionCreator, OrderRequestSender } from '../order';
+import { createPaymentClient, PaymentActionCreator, PaymentMethodActionCreator, PaymentMethodRequestSender, PaymentRequestSender, PaymentStrategyActionCreator, PaymentStrategyRegistry } from '../payment';
 import { BraintreeScriptLoader, BraintreeSDKCreator } from '../payment/strategies/braintree';
+import { GooglePayBraintreeInitializer, GooglePayPaymentStrategy, GooglePayScriptLoader } from '../payment/strategies/googlepay';
+import GooglePayPaymentProcessor from '../payment/strategies/googlepay/googlepay-payment-processor';
 import { PaypalScriptLoader } from '../payment/strategies/paypal';
+import { RemoteCheckoutActionCreator, RemoteCheckoutRequestSender } from '../remote-checkout';
+import { createShippingStrategyRegistry, ConsignmentActionCreator, ConsignmentRequestSender, ShippingStrategyActionCreator } from '../shipping';
 
-import { BraintreePaypalButtonStrategy, CheckoutButtonStrategy } from './strategies';
+import { BraintreePaypalButtonStrategy, CheckoutButtonStrategy, GooglePayBraintreeButtonStrategy } from './strategies';
 
 export default function createCheckoutButtonRegistry(
     store: CheckoutStore,
@@ -16,10 +24,27 @@ export default function createCheckoutButtonRegistry(
 ): Registry<CheckoutButtonStrategy> {
     const registry = new Registry<CheckoutButtonStrategy>();
     const scriptLoader = getScriptLoader();
+    const paymentClient = createPaymentClient(store);
     const checkoutActionCreator = new CheckoutActionCreator(
         new CheckoutRequestSender(requestSender),
         new ConfigActionCreator(new ConfigRequestSender(requestSender))
     );
+    const paymentRequestSender = new PaymentRequestSender(paymentClient);
+    const orderActionCreator = new OrderActionCreator(
+        new OrderRequestSender(requestSender),
+        new CheckoutValidator(new CheckoutRequestSender(requestSender)));
+    const paymentStrategyActionCreator = new PaymentStrategyActionCreator(
+        new PaymentStrategyRegistry(store),
+        orderActionCreator
+    );
+    const paymentActionCreator = new PaymentActionCreator(
+        paymentRequestSender,
+        orderActionCreator
+    );
+    const googlepayScriptLoader = new GooglePayScriptLoader(scriptLoader);
+    const braintreeScriptLoader = new BraintreeScriptLoader(scriptLoader);
+    const braintreeSDKCreator = new BraintreeSDKCreator(braintreeScriptLoader);
+    const paymentMethodActionCreator = new PaymentMethodActionCreator(new PaymentMethodRequestSender(requestSender));
 
     registry.register('braintreepaypal', () =>
         new BraintreePaypalButtonStrategy(
@@ -39,6 +64,23 @@ export default function createCheckoutButtonRegistry(
             new PaypalScriptLoader(scriptLoader),
             createFormPoster(),
             true
+        )
+    );
+
+    registry.register('googlepaybraintree', () =>
+        new GooglePayBraintreeButtonStrategy(
+            store,
+            new FormPoster(),
+            googlepayScriptLoader,
+            checkoutActionCreator,
+            paymentMethodActionCreator,
+            new GooglePayPaymentProcessor(
+                store,
+                paymentMethodActionCreator,
+                new GooglePayScriptLoader(scriptLoader),
+                new GooglePayBraintreeInitializer(braintreeSDKCreator),
+                new BillingAddressActionCreator(new BillingAddressRequestSender(requestSender))
+            )
         )
     );
 
---
+++ src/checkout-buttons/strategies/googlepay/googlepay-braintree-button-strategy.ts
@@ -0,0 +1,107 @@
+import { FormPoster } from '@bigcommerce/form-poster';
+
+import { Checkout, CheckoutActionCreator, CheckoutStore } from '../../../checkout';
+import { InvalidArgumentError, MissingDataError, MissingDataErrorType } from '../../../common/error/errors';
+import { bindDecorator as bind } from '../../../common/utility';
+import { PaymentInitializeOptions, PaymentMethod, PaymentMethodActionCreator } from '../../../payment';
+import { GooglePayBraintreeSDK, GooglePayPaymentOptions, GooglePayPaymentStrategy, GooglePayScriptLoader, GooglePaySDK } from '../../../payment/strategies/googlepay';
+import GooglePayPaymentProcessor from '../../../payment/strategies/googlepay/googlepay-payment-processor';
+import { CheckoutButtonInitializeOptions, CheckoutButtonOptions } from '../../checkout-button-options';
+import CheckoutButtonStrategy from '../checkout-button-strategy';
+
+import { GooglePayBraintreeButtonInitializeOptions } from './googlepay-braintree-button-options';
+
+export default class GooglePayBraintreeButtonStrategy extends CheckoutButtonStrategy {
+    private _paymentMethod?: PaymentMethod;
+    private _methodId!: string;
+    private _checkout?: Checkout;
+
+    constructor(
+        private _store: CheckoutStore,
+        private _formPoster: FormPoster,
+        private _googlePayScriptLoader: GooglePayScriptLoader,
+        private _checkoutActionCreator: CheckoutActionCreator,
+        private _paymentMethodActionCreator: PaymentMethodActionCreator,
+        private _googlePayPaymentProcessor: GooglePayPaymentProcessor
+    ) {
+        super();
+    }
+
+    initialize(options: CheckoutButtonInitializeOptions): Promise<void> {
+        if (this._isInitialized) {
+            return super.initialize(options);
+        }
+
+        const { googlepaybraintree, methodId } = options;
+
+        if (!googlepaybraintree || !methodId) {
+            throw new MissingDataError(MissingDataErrorType.MissingPaymentMethod);
+        }
+
+        this._methodId = methodId;
+
+        return this._store.dispatch(this._checkoutActionCreator.loadDefaultCheckout())
+            .then(stateCheckout => {
+                this._checkout = stateCheckout.checkout.getCheckout();
+                if (!this._checkout || !this._checkout.cart.id) {
+                    throw new MissingDataError(MissingDataErrorType.MissingCart);
+                }
+
+                return this._googlePayPaymentProcessor.initialize(this._methodId)
+                    .then(() => this._googlePayPaymentProcessor.createButton(this._handleWalletButtonClick));
+
+        }).then(() => super.initialize(options));
+    }
+
+    deinitialize(options: CheckoutButtonOptions): Promise<void> {
+        if (!this._isInitialized) {
+            return super.deinitialize(options);
+        }
+
+        this._paymentMethod = undefined;
+
+        return this._googlePayPaymentProcessor.deinitialize()
+            .then(() => super.deinitialize(options));
+    }
+
+    private _createGooglePayButton(googleClientOptions: GooglePayPaymentOptions, googlepaybraintree: GooglePayBraintreeButtonInitializeOptions): Promise<void> {
+        return this._googlePayScriptLoader.load()
+            .then(googleSDK => {
+                const googleClient = new googleSDK.payments.api.PaymentsClient(googleClientOptions);
+
+                const googlePayButton = googleClient.createButton({});
+                const container = document.querySelector(`#${googlepaybraintree.container}`);
+
+                if (!container) {
+                    throw new InvalidArgumentError('Unable to create sign-in button without valid container ID.');
+                }
+
+                container.appendChild(googlePayButton);
+            });
+    }
+
+    @bind
+    private _handleWalletButtonClick(): Promise<void> {
+        return this._googlePayPaymentProcessor.displayWallet()
+            .then(() => this._onPaymentSelectComplete());
+    }
+
+    private _onPaymentSelectComplete(): void {
+        this._formPoster.postForm('/checkout.php', {
+            headers: {
+                Accept: 'text/html',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            params: {
+                fromGooglePay: true,
+            },
+        });
+    }
+
+    private _onError(error?: Error): void {
+        if (error) {
+            throw new Error(error.message);
+        }
+    }
+
+}
