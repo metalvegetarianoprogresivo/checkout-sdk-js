de3b3974b9421ce89bb2611190004feba47d1587
Irving Gomez <lordzero0000@gmail.com>
2018-11-01 09:37:03 -0600

    feat(payments) INT-858 Addressing comments from the PR

--- src/checkout-buttons/strategies/checkout-button-method-type.ts
+++ src/checkout-buttons/strategies/checkout-button-method-type.ts
@@ -2,7 +2,7 @@ export enum CheckoutButtonMethodType {
     BRAINTREE_PAYPAL = 'braintreepaypal',
     BRAINTREE_PAYPAL_CREDIT = 'braintreepaypalcredit',
     GOOGLEPAY_BRAINTREE = 'googlepaybraintree',
-    GOOGLEPAY_STRIPE= 'googlepaystripe',
+    GOOGLEPAY_STRIPE = 'googlepaystripe',
     MASTERPASS = 'masterpass',
 
 }
--- src/checkout-buttons/strategies/index.ts
+++ src/checkout-buttons/strategies/index.ts
@@ -1,8 +1,7 @@
 export { default as BraintreePaypalButtonStrategy } from './braintree-paypal-button-strategy';
-export { GooglePayButtonStrategy } from './googlepay';
 export { default as CheckoutButtonStrategy } from './checkout-button-strategy';
 export { default as MasterpassButtonStrategy } from './masterpass-button-strategy';
 export { CheckoutButtonMethodType } from './checkout-button-method-type';
 
 export { BraintreePaypalButtonInitializeOptions } from './braintree-paypal-button-options';
-export { GooglePayButtonInitializeOptions } from './googlepay';
+export { GooglePayButtonStrategy, GooglePayButtonInitializeOptions } from './googlepay';
